452735ded6232d011ba26b995a2e35843c47ab42
Jonathan Cheng <jcheng461@gmail.com>
2022-06-14 23:45:47 +0900

    pass test where haiku object takes 3 parameters, and all parameters are strings

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,8 +1,14 @@
 import Haiku from "./../src/haiku.js";
 
 describe("Haiku", () => {
-  test("should take in a parameter from user, and tell if it is a string", () => {
-    const words = new Haiku("this is a string");
-    expect(typeof words.line1).toEqual("string");
+  test("should take in all 3 parameters from user, and tell if they are all strings", () => {
+    const threeLines = new Haiku(
+      "this is a string",
+      "second line here",
+      "a third line"
+    );
+    expect(typeof threeLines.line1).toEqual("string");
+    expect(typeof threeLines.line2).toEqual("string");
+    expect(typeof threeLines.line3).toEqual("string");
   });
 });

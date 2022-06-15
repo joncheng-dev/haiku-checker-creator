e3eb78fee635bc447d03956375704bc08801fd3a
Jonathan Cheng <jcheng461@gmail.com>
2022-06-15 23:55:46 +0900

    WIP: add function to count syllables in a line or sentence

--- src/haiku.js
+++ src/haiku.js
@@ -1,7 +1,23 @@
-export default function Haiku(line1, line2, line3) {
-  this.line1 = line1;
-  this.line2 = line2;
-  this.line3 = line3;
+export default class Haiku {
+  constructor(line1, line2, line3) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
+
+  checkValid() {}
 }
 
-Haiku.prototype.checkValid = function () {};
+function syllablesInWord(word) {
+  // If there are 3 characters or less, this is 1 syllable.
+  if (word.toLowerCase().length <= 3) {
+    return 1;
+  }
+}
+
+function countWords(sentence) {
+  let separatedWords = sentence.split(" ");
+  // Try taking the first word in this array..
+  let word = separatedWords[0].replace(/[.,\/?#!$%\^&\*;:{}=\-_`~()]/g, "");
+  syllablesInWord(word);
+}

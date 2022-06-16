e20d5a6d70292db38f87825dca12c54c8dea6648
Jonathan Cheng <jcheng461@gmail.com>
2022-06-16 23:43:02 +0900

    update html and user interface to take three lines of text from user, so that business logic can determine if is valid haiku

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/styles.css";
+import Haiku from "./haiku.js";
 
 // User Interface Logic
 $(document).ready(function () {
   // Add here
+  $("#haiku-checker-form").submit(function (event) {
+    event.preventDefault();
+    const line1 = $("#line1").val();
+    const line2 = $("#line2").val();
+    const line3 = $("#line3").val();
+    const haiku = new Haiku(line1, line2, line3);
+    const result = haiku.checkValid();
+    $("#haiku-check-result").append("<p>" + result + "</p>");
+  });
 });

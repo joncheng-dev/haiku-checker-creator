179d057dfb683e328ede42701bb3569d1417d9cb
Jonathan Cheng <jcheng461@gmail.com>
2022-06-19 23:59:03 +0900

    WIP: break up functions to handle vowel count - eventually counting syllables from a word entered

--- src/haiku.js
+++ src/haiku.js
@@ -5,7 +5,10 @@ export default class Haiku {
     this.line3 = line3;
   }
 
+  // Check valid will see if it's a valid Haiku.
   checkValid() {
+    // Check all three lines for numbers of syllables
+    // First line:
     if (syllablesInWord(this.line1) === 1) {
       return "Line 1 has one syllable.";
     } else {
@@ -14,11 +17,23 @@ export default class Haiku {
   }
 }
 
+// Counts syllables.
 function syllablesInWord(word) {
   // If there are 3 characters or less, this is 1 syllable.
   if (word.toLowerCase().length <= 3) {
     return 1;
   }
+  // If there are more than 3 characters, continue to check for syllables.
+  // Check number of vowels.
+  else if (word.toLowerCase().length > 3) {
+    return "Number of vowels is " + vowelCount(word.toLowerCase());
+  }
+}
+
+// Counts vowels
+function vowelCount(word) {
+  const count = word.match(/[aeiou]/gi).length;
+  return count;
 }
 
 function countWords(sentence) {
